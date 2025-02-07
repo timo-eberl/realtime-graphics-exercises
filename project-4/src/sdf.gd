3146a8e862efbe9a1c847443c0f57a1fbf878dda
Eberl Timo <te038@hdm-stuttgart.de>
2025-02-07 15:22:40 +0100

    image based sdf sphere that respects the depth map

--- project-4/src/sdf.gd
+++ project-4/src/sdf.gd
@@ -1,9 +1,6 @@
-extends Panel
+extends MeshInstance3D
 
 @onready var camera : Camera3D = %Camera
-
-func _ready() -> void:
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -13,9 +10,11 @@ func radians(degrees: float) -> float:
 	return degrees / 360.0 * TAU
 
 func set_shader_parameters():
-	self.material.set_shader_parameter("aspect_ratio", self.size.x / self.size.y);
+	self.material_override.set_shader_parameter(
+		"aspect_ratio", float(get_viewport().size.x) / get_viewport().size.y
+	);
 	
-	self.material.set_shader_parameter("azimuth", camera.global_rotation.y)
-	self.material.set_shader_parameter("elevation", camera.global_rotation.x)
-	self.material.set_shader_parameter("camera_position", camera.global_position)
-	self.material.set_shader_parameter("vertical_fov_radians", radians(camera.fov))
+	self.material_override.set_shader_parameter("azimuth", camera.global_rotation.y)
+	self.material_override.set_shader_parameter("elevation", camera.global_rotation.x)
+	self.material_override.set_shader_parameter("camera_position", camera.global_position)
+	self.material_override.set_shader_parameter("vertical_fov_radians", radians(camera.fov))
