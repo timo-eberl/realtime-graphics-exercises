2c7317e6a10b196b79dcbe9b5c879b87e0b70409
Eberl Timo <te038@hdm-stuttgart.de>
2025-02-14 21:07:21 +0100

    shading + add strand roundness

--- project-4/src/_fur/fur.gd
+++ project-4/src/_fur/fur.gd
@@ -4,7 +4,7 @@ extends MeshInstance3D
 @export_group("Setup")
 @export var shell_mesh: Mesh
 @export var shell_material: ShaderMaterial
-@export_range(1, 256) var shell_count: int = 16
+@export_range(1, 256) var shell_count: int = 16 # 256 is the max number of possible mesh surfaces
 @export var setup_button: bool:
 	set(value):
 		setup()
@@ -13,9 +13,14 @@ extends MeshInstance3D
 
 @export_category("Fur")
 @export_range(0, 1) var length: float = 0.15
-@export_range(0, 3) var distance_density_attenuation: float = 1
+@export_range(0, 1) var length_uniformity: float = 0.0
+@export_range(0, 3) var distance_shell_density_attenuation: float = 1
 @export_range(1, 1000) var density: float = 100
 @export_range(0, 10) var thickness: float = 5
+@export_range(0, 0.999) var strand_roundness: float = 0
+@export_category("Shading")
+@export_color_no_alpha var color: Color
+@export_range(0, 5) var occlusion_attenuation: float = 1
 
 func setup() -> void:
 	# generate mesh that has one surface for every shell
@@ -53,9 +58,13 @@ func update_materials() -> void:
 		mat.set_shader_parameter("u_shell_index", i)
 		mat.set_shader_parameter("u_shell_count", shell_count)
 		mat.set_shader_parameter("u_length", length)
-		mat.set_shader_parameter("u_distance_density_attenuation", distance_density_attenuation)
+		mat.set_shader_parameter("u_length_uniformity", length_uniformity)
+		mat.set_shader_parameter("u_distance_shell_density_attenuation", distance_shell_density_attenuation)
 		mat.set_shader_parameter("u_density", density)
 		mat.set_shader_parameter("u_thickness", thickness)
+		mat.set_shader_parameter("u_strand_roundness", strand_roundness)
+		mat.set_shader_parameter("u_color", color)
+		mat.set_shader_parameter("u_occlusion_attenuation", occlusion_attenuation)
 
 func _process(_delta: float) -> void:
 	update_materials()
